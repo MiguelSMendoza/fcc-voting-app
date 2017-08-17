7511997234e3a12243db435a1e2506d5a42b2fc6
Miguel S. Mendoza <miguel@smendoza.net>
2017-08-17 20:29:27 +0200

    Voting App Favicon and Minor Changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebase : {
-    apiKey: 'AIzaSyDFew-L28USW26903K_-STRZfdilXOBTR4',
-    authDomain: 'fcc-voting-app-55f23.firebaseapp.com',
-    databaseURL: 'https://fcc-voting-app-55f23.firebaseio.com',
-    projectId: 'fcc-voting-app-55f23',
-    storageBucket: '',
-    messagingSenderId: '521557440223'
+    apiKey: 'AIzaSyC3ADNgzymRoeMVhl326atcy5mQaFo6MyI',
+    authDomain: 'fcc-smendoza.firebaseapp.com',
+    databaseURL: 'https://fcc-smendoza.firebaseio.com',
+    projectId: 'fcc-smendoza',
+    storageBucket: 'fcc-smendoza.appspot.com',
+    messagingSenderId: '82796816871'
   }
 };
